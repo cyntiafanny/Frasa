9fa8b40af75d8a532260d4ef8f3ee74c27d713ac
Aaron Effendi <effendiaaron@gmail.com>
2020-04-22 14:29:19 +0700

    second

---
+++ app/src/main/java/com/labill/frasa/getstarted.java
@@ -0,0 +1,16 @@
+package com.labill.frasa;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.labill.frasa.ui.login.LoginViewModel;
+
+public class getstarted extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.getstarted);
+    }
+}
